88994a9617c1ddb71b0084909529ac5eebd796c7
Samuel <samworlds1231337@gmail.com>
2024-11-01 14:13:24 -0400

    nvm now it works... insert doesnt work with the util.py function

--- HW3/main.py
+++ HW3/main.py
@@ -49,19 +49,18 @@ def index():
 @app.route('/api/update_basket_a/')
 def update_basket_a():
     cursor, connection = util.connect_to_db(username,password,host,port,database)
-
-    record = util.run_and_fetch_sql(cursor,"INSERT INTO basket_a VALUES (5, 'Cherry');")
     
-
-    if record == -1:     
-        record = f"{record} something is wrong or the data already exist in the database"
-    else:
+    record = ''
+    try:
+        cursor.execute("INSERT INTO basket_a VALUES (5, 'Cherry');")
+        cursor.connection.commit()
         record = "Success!"
-        
+    except(Exception, util.Error) as error:
+        record = f"Error while executing SQL code: {error}"
+
     # disconnect from database
     util.disconnect_from_db(connection,cursor)
-    # using render_template function, Flask will search
-    # the file named index.html under templates folder
+    
     return render_template('update_basket_a.html', output = record)
 
 
